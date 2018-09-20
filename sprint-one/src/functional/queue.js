89f00e4971815943722d25221c1ecd5ae7e1051a
EthanVV <ethan.vanvleck@gmail.com>
2018-09-20 13:49:16 -0500

    functional queue working

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,20 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size() + someInstance.removed] = value;
   };
 
   someInstance.dequeue = function() {
+    var holder = storage[someInstance.removed];
+    delete storage[someInstance.removed];
+    someInstance.removed++;
+    return holder;
   };
 
+  someInstance.removed = 0;
+
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
